ceba1d2c241ff0b6b62f6f6a65234a0c6099cf7a
deptster <deepantripathi0000@gmail.com>
2022-04-26 17:11:42 +0530

    question 1 done

--- exp.js
+++ exp.js
@@ -1,2 +1,22 @@
-const data = require('./data.json');
-console.log(data.firstname);
+const mongoose = require('mongoose');
+const url = require("./database")
+
+
+// const data = require('./data.json');
+// console.log(data.firstname);
+
+
+mongoose.connect(url, function(err, db) {  
+    if (err) throw err; 
+
+db.collection("Users").deleteMany({}, function(err, result) {  
+    if (err) throw err;  
+    console.log("deleted Users");  
+  });  
+
+  db.collection("UsersProfile").deleteMany({}, function(err, result) {  
+    if (err) throw err;  
+    console.log("deleted UsersProfile"); 
+    db.close();  
+  }); 
+});
