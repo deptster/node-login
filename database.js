253086c9b397cb5847ef09b8600b2d59dfc5a7e7
deptster <deepantripathi0000@gmail.com>
2022-04-27 12:50:46 +0530

    Question 2 completed

--- database.js
+++
@@ -1,7 +0,0 @@
-var dbUserName = "dept";
-var dbPassword = "v9f4y9PIIDDNB4Q4"
-var dbName = "login"
-
-var url = "mongodb+srv://" + dbUserName + ":" + dbPassword + "@cluster1.vwm8s.mongodb.net/" + dbName +"?retryWrites=true&w=majority"; 
-
-module.exports = url;
